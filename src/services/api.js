391a03b063897f11bb9eeda6f413db0e22b65f29
Rahul-shastri67 <shastrirahul246@gmail.com>
2025-12-11 11:18:24 +0530

    point frontend to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://crud-backend-1-n725.onrender.com/',
 });
 
 export const getUsers = () => API.get('/users');
